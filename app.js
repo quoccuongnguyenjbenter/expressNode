6bc8d39442cac9687b6988bbe0d00b34cfee0ea0
Nguyen_Quoc_Cuong <quoccuongdn96vn@gmail.com>
2019-12-26 16:51:24 +0700

    Learn Socket.io and Nodejs in Khoa Pham

--- app.js
+++ app.js
@@ -4,5 +4,5 @@ var server=require("http").createServer(app);
 server.listen(3000);
 
 app.get("/",function(req,res){
-    res.send("<font color=red>Nguyễn Quốc Cường</font>");
+    res.send("<font color=yellow>Nguyễn Quốc Cường</font>");
 });
